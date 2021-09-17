208f9b5102a88f53d9511edcece2199c60745080
iuliia <pisarev-vlg@mail.ru>
2021-09-17 17:26:57 +0300

    feat: finished IIsuService.cs

--- Isu/Services/IIsuService.cs
+++ Isu/Services/IIsuService.cs
@@ -1,18 +1,21 @@
-﻿namespace Isu.Services
+﻿using System;
+using System.Collections.Generic;
+
+namespace Isu.Services
 {
     public interface IIsuService
     {
-        //Group AddGroup(string name);
-        //Student AddStudent(Group group, string name);
+        Group AddGroup(string name);
+        Student AddStudent(Group group, string name);
 
-        //Student GetStudent(int id);
-        //Student FindStudent(string name);
-        //List<Student> FindStudents(string groupName);
+        // Student GetStudent(int id);
+        Student FindStudent(string name);
+        List<Student> FindStudents(string groupName);
         //List<Student> FindStudents(CourseNumber courseNumber);
 
-        //Group FindGroup(string groupName);
-        //List<Group> FindGroups(CourseNumber courseNumber);
+        Group FindGroup(string groupName);
+        List<Group> FindGroups(CourseNumber courseNumber);
 
-        //void ChangeStudentGroup(Student student, Group newGroup);
+        void ChangeStudentGroup(Student student, Group newGroup);
     }
 }
