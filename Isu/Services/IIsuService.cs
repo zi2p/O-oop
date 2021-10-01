36fe0db2f72a1c14cdaf14751be9e91b6b3a1803
iuliia <pisarev-vlg@mail.ru>
2021-10-01 23:20:30 +0300

    x3

--- Isu/Services/IIsuService.cs
+++ Isu/Services/IIsuService.cs
@@ -8,15 +8,12 @@ namespace Isu.Services
     {
         Group AddGroup(string name);
         Student AddStudent(Group group, string name);
-
         Student GetStudent(int id);
         Student FindStudent(string name);
         List<Student> FindStudents(string groupName);
         List<Student> FindStudents(CourseNumber courseNumber);
-
         Group FindGroup(string groupName);
         List<Group> FindGroups(CourseNumber courseNumber);
-
         void ChangeStudentGroup(Student student, Group newGroup);
     }
 }
